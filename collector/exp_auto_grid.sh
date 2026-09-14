28692d7b3c213444fa8769e28bdc11c1db81ca6c
JacopoCarlon <jacopo.equi@gmail.com>
2026-09-14 15:08:25 +0200

    prepared to run on whiskey-lace : makefile, mac, serverAccess, testing all cstates enabled.

--- collector/exp_auto_grid.sh
+++ collector/exp_auto_grid.sh
@@ -16,33 +16,34 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # this needs to be updated betwen whiskey and treebeard
 
 ##  ##  ##  
-### --- RETIS treebeard-cplex3 config:
-# Server B Access (treebeard)
-SERVER_B_USER="jcarlon"
-SERVER_B_HOST="cplex3"
-SERVER_B_SCRIPT_DIR="/home/jcarlon/test/dpdk-tester/generator"
-# Server A Configuration (cplex3)
-##  L3FWD_PATH="/home/jcarlon/zzz_test/dpdk/build/examples/dpdk-l3fwd-power"
-L3FWD_PATH="${SCRIPT_DIR}/../../dpdk/build/examples/dpdk-l3fwd-power"
-RAPL_SCRIPT="${SCRIPT_DIR}/run_rapl.sh"
-RESULTS_DIR="${SCRIPT_DIR}/res" 
-MEASUREMENT_DURATION=5
-MAX_BITRATE=10000000000  # 10 Gbps in bits/sec
-
-
-
-#   ##  ##  ##  
-#   ### --- UNIPI whiskey-lace config:
-#   ### --- Server B Access --- --- --- --- 
-#   SERVER_B_USER="carlon"
-#   SERVER_B_HOST="lace"
-#   SERVER_B_SCRIPT_DIR="/home/carlon/test/dpdk-tester/generator/"
-#   ### --- Server A Configuration --- --- 
+#   ### --- RETIS treebeard-cplex3 config:
+#   # Server B Access (treebeard)
+#   SERVER_B_USER="jcarlon"
+#   SERVER_B_HOST="cplex3"
+#   SERVER_B_SCRIPT_DIR="/home/jcarlon/test/dpdk-tester/generator"
+#   # Server A Configuration (cplex3)
+#   ##  L3FWD_PATH="/home/jcarlon/zzz_test/dpdk/build/examples/dpdk-l3fwd-power"
 #   L3FWD_PATH="${SCRIPT_DIR}/../../dpdk/build/examples/dpdk-l3fwd-power"
 #   RAPL_SCRIPT="${SCRIPT_DIR}/run_rapl.sh"
 #   RESULTS_DIR="${SCRIPT_DIR}/res" 
-#   MEASUREMENT_DURATION=10
-#   MAX_BITRATE=400000000000  # 40 Gbps in bits/sec
+#   MEASUREMENT_DURATION=5
+#   MAX_BITRATE=10000000000  # 10 Gbps in bits/sec
+
+
+
+##  ##  ##  
+### --- UNIPI whiskey-lace config:
+### --- Server B Access --- --- --- --- 
+SERVER_B_USER="carlon"
+SERVER_B_HOST="lace"
+SERVER_B_SCRIPT_DIR="/home/carlon/test/dpdk-tester/generator/"
+### --- Server A Configuration (whiskey) --- --- 
+##  L3FWD_PATH="/home/carlon/dpdk/build/examples/dpdk-l3fwd-power"
+L3FWD_PATH="${SCRIPT_DIR}/../../dpdk/build/examples/dpdk-l3fwd-power"
+RAPL_SCRIPT="${SCRIPT_DIR}/run_rapl.sh"
+RESULTS_DIR="${SCRIPT_DIR}/res" 
+MEASUREMENT_DURATION=10
+MAX_BITRATE=400000000000  # 40 Gbps in bits/sec
 
 
 
